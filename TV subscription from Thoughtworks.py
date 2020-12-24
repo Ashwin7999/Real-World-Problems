099e7a8a7e194381e5dcc3afff1f4576dec7ec0b
Ashwina7999 <59051731+Ashwina7999@users.noreply.github.com>
2020-12-24 08:50:10 +0530

    Update TV subscription from Thoughtworks.py

--- TV subscription from Thoughtworks.py
+++ TV subscription from Thoughtworks.py
@@ -1,3 +1,58 @@
+                                                                    Channel Subscription
+As a cable TV operator, I want to provide my customers the ability to subscribe to a channel of their choice and upgrade to different packs based on their preference. 
+A customer has a certain balance in his/her account and wishes to subscribe to a channel. The channel should get added to the list of subscribed channels for the customer.
+A customer might also wish to upgrade an existing channel he/she has already subscribed to. This should be allowed if an upgrade is possible for the channel. All of the
+above should be allowed as long as the customer has the required minimum balance needed for subscription/upgrade. After the subscription/upgrade the difference amount 
+should be reflected immediately in the customer’s account.
+
+Hardcoded Data:
+List of channels, with the possible upgrades. Eg:
+    
+ABC Channel:
+Base: 120 INR
+SD: 130 INR
+HD: 150 INR
+4k: 200 INR
+Allowed Locations: USA, Canada
+    
+PQR Channel:
+Base: 120 INR
+SD: 130 INR
+Allowed Locations: All
+    
+Customer Details:
+Name: John Doe
+Account Balance: 1000 INR
+Currently Subscribed Channels: PQR (Base)
+    
+Input:
+Do you want to subscribe or upgrade?
+Subscribe
+Enter Channel Name
+ABC
+Please choose subscription type: (Base, SD, HD, 4k)
+4k
+
+O/p:
+Channel ABC (4k) subscribed successfully.
+Available account balance: 800 INR
+Continue? <y/n>
+Y
+
+Input:
+Do you want to subscribe or upgrade?
+Upgrade
+Enter Channel Name
+PQR
+Please choose subscription type: (Base, SD)
+SD
+
+O/p:
+Channel PQR (SD) subscribed successfully.
+Available account balance: 790 INR
+
+
+PROGRAM:
 class Customer:
     count=0
     def __init__(self):
